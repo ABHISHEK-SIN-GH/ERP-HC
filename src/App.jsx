434f8fa30505842a7996c99af8115222480049f0
Abhishek Kumar Singh <singh.abhishek151019@gmail.com>
2024-03-06 00:53:04 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom"
+import Auth from "./pages/Auth"
+
+function App() {
+
+  return (
+    <>
+      <Outlet/>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Auth from './pages/Auth.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>,
+    children:[
+      {
+        path:'/auth',
+        element:<Auth/>
+      },
+      {
+        path:'/dashboard',
+        element:<Dashboard/>
+      },
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />,
+)
