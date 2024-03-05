e99a4ecf71a66fc9d7ef7038c0deab43a4f4ef50
Abhishek Kumar Singh <singh.abhishek151019@gmail.com>
2024-03-06 02:07:18 +0530

    intial commit 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { Outlet } from "react-router-dom"
 import Auth from "./pages/Auth"
+import "./styles/mobile.css"
+import "./styles/tablet.css"
+import "./styles/smallLaptop.css"
+import "./styles/laptop.css"
+import "./styles/desktop.css"
 
 function App() {
 
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -12,30 +12,41 @@ import DiagnosisImg from "../assets/dashboard/Diagnosis.png";
 
 export default function Dashboard() {
   const menuItems = [
-    {nav:"IPD",bgColor:'bg-gray-100',bgImg:IPDImg},
-    {nav:"Accounts",bgColor:'bg-blue-100',bgImg:AccountsImg},
-    {nav:"Inventory",bgColor:'bg-green-100',bgImg:InventoryImg},
-    {nav:"HRMS",bgColor:'bg-yellow-100',bgImg:HRMSImg},
-    {nav:"POS",bgColor:'bg-red-100',bgImg:POSImg},
-    {nav:"Diagnosis",bgColor:'bg-purple-100',bgImg:DiagnosisImg},
-    {nav:"Front Desk",bgColor:'bg-indigo-100',bgImg:FrontdeskImg},
-    {nav:"Pharmacy",bgColor:'bg-pink-100',bgImg:PharmacyImg},
-    {nav:"OPD",bgColor:'bg-teal-100',bgImg:OPDImg},
-    {nav:"VAYAM ERP",bgColor:'bg-orange-100',bgImg:null}
+    { nav: "IPD", bgColor: "bg-gray-100", bgImg: IPDImg },
+    { nav: "Accounts", bgColor: "bg-blue-100", bgImg: AccountsImg },
+    { nav: "Inventory", bgColor: "bg-green-100", bgImg: InventoryImg },
+    { nav: "HRMS", bgColor: "bg-yellow-100", bgImg: HRMSImg },
+    { nav: "POS", bgColor: "bg-red-100", bgImg: POSImg },
+    { nav: "Diagnosis", bgColor: "bg-purple-100", bgImg: DiagnosisImg },
+    { nav: "Front Desk", bgColor: "bg-indigo-100", bgImg: FrontdeskImg },
+    { nav: "Pharmacy", bgColor: "bg-pink-100", bgImg: PharmacyImg },
+    { nav: "OPD", bgColor: "bg-teal-100", bgImg: OPDImg },
+    { nav: "VAYAM ERP", bgColor: "bg-orange-100", bgImg: null },
   ];
 
   return (
     <div className="circular-menu">
       {menuItems.map((item, index) => (
         <Link className="menu-item">
-            <span className={`h-24 w-24 md:h-28 md:w-28 2xl:h-40 2xl:w-40 absolute rounded-full inset-0 translate-x-1.5 translate-y-1.5 ${item.bgColor} transition-transform group-hover:translate-x-0 group-hover:translate-y-0`}></span>
-            <span title={item.nav} className="h-24 w-24 md:h-28 md:w-28 2xl:h-40 2xl:w-40 relative rounded-full flex border-2 border-current px-8 py-3 text-sm font-extrabold md:font-bold uppercase tracking-widest text-black group-active:text-opacity-75 justify-center items-center">
-              {
-              (item.bgImg) ? 
-              <img src={item.bgImg}/> :
-              item.nav
-              }
-            </span>
+          <span
+            className={`
+            h-24 w-24 
+            md:h-28 md:w-28 
+            lg:h-32 lg:w-32
+            2xl:h-40 2xl:w-40 
+            absolute rounded-full inset-0 translate-x-1.5 translate-y-1.5 ${item.bgColor} transition-transform group-hover:translate-x-0 group-hover:translate-y-0`}
+          ></span>
+          <span
+            title={item.nav}
+            className="
+            h-24 w-24 
+            md:h-28 md:w-28 
+            lg:h-32 lg:w-32
+            2xl:h-40 2xl:w-40
+            relative rounded-full flex border-2 border-current px-8 py-3 text-sm font-extrabold md:font-bold uppercase tracking-widest text-black group-active:text-opacity-75 justify-center items-center"
+          >
+            {item.bgImg ? <img src={item.bgImg} /> : item.nav}
+          </span>
         </Link>
       ))}
     </div>
